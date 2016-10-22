6eef0d7dd3478346c7d6d08bd794c71d61225c0c
andrewjmeng@gmail.com <andrewjmeng@gmail.com>
2016-10-22 14:35:32 -0700

    (feat) Add Text Area

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import styles from './app.css';
 import React, {Component} from 'react';
+import Board from './board';
 
 export default class App extends Component {
   render() {
-    return <h1 className={styles.app}>Hello world!</h1>;
+    return (
+      <div>
+        <h1 className={styles.app}>Hello world!</h1>
+        <Board />
+      </div>
+    )
   }
 };
