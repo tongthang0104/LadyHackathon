e3f9b424359103d5e1f9f8d3696daa72e65ece3f
Timothy Shiu <timoweave@gmail.com>
2016-10-22 17:32:51 -0700

    (database) add mongo and update config

---
+++ models.js
@@ -0,0 +1,55 @@
+const config = require('./config');
+const mongoose = require('mongoose');
+var q = require('q');
+var Schema = mongoose.Schema;
+
+mongoose.Promise = q.Promise; // NOTE: original mongoose.mpromise is deprecated
+
+var WordsSchema = Schema({
+  // category : { type : String, required :  false },
+  words: [ { type: String, required: false } ]
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+var models = {
+  words : mongoose.model('words', WordsSchema)
+};
+
+let did_crudify = false;
+if (!did_crudify) {
+  crudify_models();
+  did_crudify = true;
+}
+
+module.exports = models;
+
+function crudify_models() {
+
+  mongoose.connection.on('connected', connected);
+  mongoose.connection.on('disconnected', disconnected);
+  mongoose.connection.on('error', error);
+  mongoose.connection.once('open', ready);
+
+  var db = mongoose.connect(config.mlab);
+  var mlab = config.mlab;
+
+  function ready() {
+    console.log("mongoose ready!");
+  }
+
+  function connected() {
+    console.log('OK', 'connected', 'mongoose server', mlab);
+  }
+
+  function disconnected() {
+    console.log('OK', 'disconnected', 'mongoose server', mlab);
+  }
+
+  function error() {
+    console.log('NO', 'error', 'mongoose server', mlab);
+  }
+}
